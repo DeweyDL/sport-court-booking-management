f84529a85c8e473eb186d8466f2bb6c11dbdcbd6
Le Van Duy <levanduy3122006@gmail.com>
2026-04-23 17:43:04 +0700

    add TRIGGERS.sql and PROCCEDURES_FUNCTIONS.sql into resources/db

---
+++ src/main/resources/db/PROCEDURES_FUNCTIONS.sql
@@ -0,0 +1,147 @@
+CREATE OR REPLACE PROCEDURE PRC_CAP_NHAT_SO_TIEN_HOA_DON(
+    P_MAHD IN HOA_DON.MAHD%TYPE
+)
+AS
+    V_MAKH          HOA_DON.MAKH%TYPE;
+    V_GIAMGIA       HOA_DON.GIAMGIA%TYPE  := 0;
+    V_TIEN_COC      HOA_DON.TIEN_COC%TYPE := 0;
+    V_MA_HANG       KHACH_HANG.MA_HANG%TYPE;
+    V_CHIET_KHAU    NUMBER(12, 2)         := 0;
+    V_TIEN_THUE_SAN NUMBER(12, 2)         := 0;
+    V_TIEN_DICH_VU  NUMBER(12, 2)         := 0;
+    V_TONG_GIA_TRI  NUMBER(12, 2)         := 0;
+    V_GIAM_HANG     NUMBER(12, 2)         := 0;
+    V_GIAM_HOA_DON  NUMBER(12, 2)         := 0;
+    V_TONG_TIEN     NUMBER(12, 2)         := 0;
+BEGIN
+
+    SELECT MAKH, NVL(GIAMGIA, 0), NVL(TIEN_COC, 0)
+    INTO V_MAKH, V_GIAMGIA, V_TIEN_COC
+    FROM HOA_DON
+    WHERE MAHD = P_MAHD
+      AND IS_DELETED = 0;
+
+    SELECT NVL(SUM(BG.GIA), 0)
+    INTO V_TIEN_THUE_SAN
+    FROM CHI_TIET_HOA_DON_THUE_SAN CT
+             JOIN SAN_CON SC
+                  ON SC.MASAN = CT.MASAN
+                      AND SC.IS_DELETED = 0
+             JOIN BANG_GIA BG
+                  ON BG.MAKV = SC.MAKV
+                      AND BG.MAKG = CT.MAKG
+                      AND BG.IS_DELETED = 0
+    WHERE CT.MAHD = P_MAHD
+      AND CT.IS_DELETED = 0
+      AND CT.TRANGTHAI <> 'ĐÃ HUỶ';
+
+    SELECT NVL(SUM(
+                       CASE
+                           WHEN CT.MASP IS NOT NULL THEN CT.SL * SP.GIA
+                           WHEN CT.MADC IS NOT NULL THEN CT.SL * DC.GIA
+                           ELSE 0
+                           END
+               ), 0)
+    INTO V_TIEN_DICH_VU
+    FROM CHI_TIET_HOA_DON_DICH_VU_DA_DUNG CT
+             LEFT JOIN SAN_PHAM SP
+                       ON SP.MASP = CT.MASP
+                           AND SP.IS_DELETED = 0
+             LEFT JOIN DUNG_CU_THE_THAO DC
+                       ON DC.MADC = CT.MADC
+                           AND DC.IS_DELETED = 0
+    WHERE CT.MAHD = P_MAHD
+      AND CT.IS_DELETED = 0;
+
+    BEGIN
+        SELECT KH.MA_HANG
+        INTO V_MA_HANG
+        FROM KHACH_HANG KH
+        WHERE KH.MAKH = V_MAKH
+          AND KH.IS_DELETED = 0;
+
+        IF V_MA_HANG IS NOT NULL THEN
+            BEGIN
+                SELECT NVL(HK.CHIET_KHAU, 0)
+                INTO V_CHIET_KHAU
+                FROM HANG_KHACH_HANG HK
+                WHERE HK.MA_HANG = V_MA_HANG
+                  AND HK.IS_DELETED = 0;
+            EXCEPTION
+                WHEN NO_DATA_FOUND THEN
+                    V_CHIET_KHAU := 0;
+            END;
+        END IF;
+    EXCEPTION
+        WHEN NO_DATA_FOUND THEN
+            V_CHIET_KHAU := 0;
+    END;
+
+    V_TONG_GIA_TRI := ROUND(V_TIEN_THUE_SAN + V_TIEN_DICH_VU, 2);
+    V_GIAM_HANG := ROUND(V_TIEN_THUE_SAN * V_CHIET_KHAU / 100, 2);
+    V_GIAM_HOA_DON := ROUND((V_TONG_GIA_TRI - V_GIAM_HANG) * V_GIAMGIA / 100, 2);
+    V_TONG_TIEN := ROUND(V_TONG_GIA_TRI - V_GIAM_HANG - V_GIAM_HOA_DON - V_TIEN_COC, 2);
+
+    IF V_TONG_TIEN < 0 THEN
+        RAISE_APPLICATION_ERROR(-20059, 'TONGTIEN khong duoc am.');
+    END IF;
+
+    UPDATE HOA_DON
+    SET TONGGIATRI = V_TONG_GIA_TRI,
+        TONGTIEN   = V_TONG_TIEN
+    WHERE MAHD = P_MAHD;
+    EXCEPTION
+    WHEN NO_DATA_FOUND THEN
+        NULL;
+END;
+/
+
+CREATE OR REPLACE PROCEDURE PRC_DIEU_CHINH_TON_SAN_PHAM(
+    P_MASP IN SAN_PHAM.MASP%TYPE,
+    P_DELTA IN NUMBER
+)
+AS
+    V_SL_TON SAN_PHAM.SL_TON%TYPE;
+BEGIN
+    SELECT SL_TON
+    INTO V_SL_TON
+    FROM SAN_PHAM
+    WHERE MASP = P_MASP
+      AND IS_DELETED = 0
+        FOR UPDATE;
+
+    IF V_SL_TON + P_DELTA < 0 THEN
+        RAISE_APPLICATION_ERROR(-20090, 'So luong ton san pham khong du.');
+    END IF;
+
+    UPDATE SAN_PHAM
+    SET SL_TON = SL_TON + P_DELTA
+    WHERE MASP = P_MASP
+      AND IS_DELETED = 0;
+END;
+/
+
+CREATE OR REPLACE PROCEDURE PRC_DIEU_CHINH_TON_DUNG_CU(
+    P_MADC IN DUNG_CU_THE_THAO.MADC%TYPE,
+    P_DELTA IN NUMBER
+)
+AS
+    V_SL_TON DUNG_CU_THE_THAO.SL_TON%TYPE;
+BEGIN
+    SELECT SL_TON
+    INTO V_SL_TON
+    FROM DUNG_CU_THE_THAO
+    WHERE MADC = P_MADC
+      AND IS_DELETED = 0
+        FOR UPDATE;
+
+    IF V_SL_TON + P_DELTA < 0 THEN
+        RAISE_APPLICATION_ERROR(-20091, 'So luong ton dung cu khong du.');
+    END IF;
+
+    UPDATE DUNG_CU_THE_THAO
+    SET SL_TON = SL_TON + P_DELTA
+    WHERE MADC = P_MADC
+      AND IS_DELETED = 0;
+END;
+/
