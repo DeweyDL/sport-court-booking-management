d47c1ad4bf3c528908ec9c11a65132dc4f2a5fe5
Le Van Duy <levanduy3122006@gmail.com>
2026-04-24 12:20:15 +0700

    rename methods CourtDAO and create CourtDAOImpl

--- src/main/java/com/sportcourt/court/dao/CourtDAO.java
+++ src/main/java/com/sportcourt/court/dao/CourtDAO.java
@@ -10,11 +10,18 @@ import java.util.Optional;
 
 public interface CourtDAO {
     List<CourtTableRow> findByCriteria(CourtSearchCriteria criteria) throws SQLException;
-    Optional<Court> findByInBranch(String courtId, String branchId) throws SQLException;
-    boolean isExistedById(String courtId) throws SQLException;
-    boolean isAreaBelongToBranch(String  AreaId, String branchId) throws SQLException;
-    boolean isRental(String courtId, String branchId) throws SQLException;
+
+    Optional<Court> findIdByInBranch(String courtId, String branchId) throws SQLException;
+
+    boolean existsById(String courtId) throws SQLException;
+
+    boolean areaBelongToBranch(String AreaId, String branchId) throws SQLException;
+
+    boolean hasActiveRental(String courtId, String branchId) throws SQLException;
+
     void insert(Court court) throws SQLException;
+
     boolean update(Court court, String branchId) throws SQLException;
+
     boolean softDelete(String courtId, String branchId) throws SQLException;
 }
