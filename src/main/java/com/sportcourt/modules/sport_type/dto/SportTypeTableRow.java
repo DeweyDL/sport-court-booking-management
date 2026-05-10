9363f4469d0c14b3f5a167ace29d7382ff3afd4b
Le Van Duy <levanduy3122006@gmail.com>
2026-05-10 23:20:30 +0700

    fix procedures functions

---
+++ src/main/java/com/sportcourt/modules/sport_type/dto/SportTypeTableRow.java
@@ -0,0 +1,12 @@
+package com.sportcourt.modules.sport_type.dto;
+
+import java.time.LocalDateTime;
+
+public class SportTypeTableRow {
+    private String sportId;
+    private String name;
+    private String description;
+    private LocalDateTime createdAt;
+
+
+}
