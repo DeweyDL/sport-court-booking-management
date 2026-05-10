e8176f60b88e8bb53856c5086955115fae55cdb6
Le Van Duy <levanduy3122006@gmail.com>
2026-05-10 23:20:30 +0700

    fix procedures functions

---
+++ src/main/java/com/sportcourt/modules/sport_type/dao/SportTypeDAO.java
@@ -0,0 +1,8 @@
+package com.sportcourt.modules.sport_type.dao;
+
+import com.sportcourt.modules.sport_type.entity.SportType;
+
+public interface SportTypeDAO {
+    void insert(SportType sportType);
+    void update()
+}
