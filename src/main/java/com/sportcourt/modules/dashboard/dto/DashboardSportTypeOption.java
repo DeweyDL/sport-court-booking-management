4acca8a265c51a006efe77c801018f63be6ac4ab
YenNhi392 <nhilee356@gmail.com>
2026-05-14 18:29:45 +0700

    add dashboard fe, be

---
+++ src/main/java/com/sportcourt/modules/dashboard/dto/DashboardSportTypeOption.java
@@ -0,0 +1,11 @@
+package com.sportcourt.modules.dashboard.dto;
+
+/**
+ * Sport type option shown on Dashboard filter bar.
+ */
+public record DashboardSportTypeOption(
+        String sportTypeId,
+        String sportTypeName
+) {
+}
+
